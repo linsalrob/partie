50757c817dfe5e87dc73dbd4f5806c17dcb852d7
linsalrob <raedwards@gmail.com>
2018-12-10 08:37:00 -0800

    updating and adding sizes

--- partie_download.sh
+++ partie_download.sh
@@ -27,6 +27,9 @@ cd $DATE
 curl --head https://s3.amazonaws.com/starbuck1/sradb/SRAmetadb.sqlite.gz > timestamp.txt
 grep Last-Modified timestamp.txt | sed -e 's/Last-Modified: //' > ~/GitHubs/partie/SRA_Update_Time
 
+curl -Lo SRA_Accessions.tab ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Accessions.tab
+
+
 # Download one of the SRA SQLite databases:
 echo "Downloading and extracting the new SQL Lite data base"
 curl -Lo SRAmetadb.sqlite.gz "https://s3.amazonaws.com/starbuck1/sradb/SRAmetadb.sqlite.gz"
@@ -51,32 +54,48 @@ sort -u sci_name_metagenome.notamplicons.ids source_metagenomic.notamplicons.ids
 
 # look at the previously downloaded metagenomes
 echo "Figuring out the new metagenomes to download"
-cut -f 1 ~/GitHubs/partie/SRA_Metagenome_Types.txt | grep -Fxvf - SRA-metagenomes.txt > SRA-metagenomes-ToDownload.txt
+cut -f 1 ~/GitHubs/partie/SRA_Metagenome_Types.tsv | grep -Fxvf - SRA-metagenomes.txt > SRA-metagenomes-ToDownload.txt
 
 # now set up a cluster job to parse out some data
 mkdir partie
 cp SRA-metagenomes-ToDownload.txt partie/
 cd partie
+
+# how many jobs do we have?
+COUNT=$(wc -l SRA-metagenomes-ToDownload.txt | awk '{print $1}')
+
 echo -e "SRA=\$(head -n \$SGE_TASK_ID SRA-metagenomes-ToDownload.txt | tail -n 1);\nperl \$HOME/partie/partie.pl -noheader \${SRA}.sra;" > partie.sh
 # and submit a few jobs to the queue to test
-mkdir sge_out sge_err
-
+# NOTE:
+# Do not make the outputs a directory!!
+# If you use a file for STDERR/STDOUT then you don't need to concatenate the outputs in the next step
 
 # this deals with an error in the BASH_FUNC_module
 unset module
-# how many jobs do we have?
-COUNT=$(wc -l SRA-metagenomes-ToDownload.txt | awk '{print $1}')
 
 if [ $HOST == "anthill" ]; then 
 	# we can submit directly
 	echo "submitting the partie job"
-	qsub -V -cwd -t 1-$COUNT:1  -o sge_out/ -e sge_err/ ./partie.sh
+	qsub -V -cwd -t 1-$COUNT:1  -o sge_out -e sge_err ./partie.sh
 else 
 	# submit via ssh
 	WD=$PWD
 	echo "Running the partie command on anthill"
-	ssh anthill "cd $PWD; qsub -V -cwd -t 1-$COUNT:1  -o sge_out/ -e sge_err/ ./partie.sh"
+	ssh anthill "cd $WD; qsub -V -cwd -t 1-$COUNT:1  -o sge_out -e sge_err ./partie.sh"
 fi
+
+# get the sizes of the metagenomes
+IDX=0;
+while [ $IDX -lt $COUNT ]; do
+	IDX=$((IDX+250));
+	echo "Getting the sizes of the metagenomes upto number $IDX of $COUNT"; 
+	head -n $IDX SRA-metagenomes-ToDownload.txt | tail -n 250 > temp;
+	epost -db sra -input temp -format acc | esummary -format runinfo -mode xml | xtract -pattern Row -element Run,bases,spots,spots_with_mates,avgLength,size_MB >> SRA_Metagenome_Sizes.tsv;
+done
+rm -f temp
+
+sort -u SRA_Metagenome_Sizes.tsv >> ~/GitHubs/partie/SRA_Metagenome_Sizes.tsv
+
 
 echo "We have submitted the PARTIE jobs to the cluster, and you need to let them run."
 echo "Once they are run (which doesn't take that long), you should be able to use the script:"
--- partie_parse_output.sh
+++ partie_parse_output.sh
@@ -18,8 +18,12 @@ WD=$PWD
 DATE=`date +%b_%Y`
 cd $DATE/partie
 
+# clean up the output. This means that we delete a few that we need to add next time
+perl -ne 'next unless (/^[DSE]RR\d+.sra\t[\d\.]+\t[\d\.]+\t[\d\.]+\t[\d\.]+$/); @a=split /\t/; next unless ($#a == 4); print' sge_out > output_clean
+
 # cat the header and the files in one go:
-head -n 1 ~/partie/SRA_PARTIE_DATA.txt | sed -e 's/\s\+PARTIE_Annotation//'  | cat - sge_out/* | grep -vP '0\t0\t0\t0' > partie_${DATE}.txt
+head -n 1 ~/partie/SRA_PARTIE_DATA.txt | sed -e 's/\s\+PARTIE_Annotation//'  | cat - output_clean | grep -vP '\t0\t0\t0\t0' > partie_${DATE}.txt
+grep -P '\t0\t0\t0\t0' sge_out | perl -pe 'chomp; s/$/\tNO DATA\n/' >> ~/partie/SRA_PARTIE_DATA.txt
 
 # run the classification
 Rscript ~/partie/RandomForest/PARTIE_Classification.R partie_${DATE}.txt
@@ -29,8 +33,8 @@ perl -npe 's/\.sra//; s/"//g; s/\,/\t/g; ' partie_classification.csv | grep -v P
 
 cd ~/partie/
 # creat the last file
-cut -f 1,6 SRA_PARTIE_DATA.txt > SRA_Metagenome_Types.txt
+cut -f 1,6 SRA_PARTIE_DATA.txt > SRA_Metagenome_Types.tsv
 
-git commit -m 'Updatine PARTIE data' -a; git push
+git commit -m 'Updating PARTIE data' -a; git push
 
 cd $WD
